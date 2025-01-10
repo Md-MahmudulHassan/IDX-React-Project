8367fb7f9eac06a4dfee90e09738a2e5e15f4f72
MD. Mahmudul Hassan <md.mahmudul.hassan.0202@gmail.com>
2025-01-10 21:02:38 +0600

    add footer

--- src/Pages/shop.jsx
+++ src/Pages/shop.jsx
@@ -3,15 +3,17 @@ import {Hero} from '../Components/hero/hero'
 import {Popular} from '../Components/popular/popular'
 import {New_collection} from '../Components/new_collection/new-collection'
 import{Offer} from '../Components/offers/offer'
-
+import {Footer} from '../Components/footer/footer'
 
 export const Shop = () => {
   return (
     <div>
       <Hero />
       <Popular />
-      <Offer />
       <New_collection />
+      <Offer />
+      <Footer />
+      
     </div>
   )
 }
